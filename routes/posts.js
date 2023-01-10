00c8a9c8e86c60f5220cd913eba573c97e36c467
rathak18 <rajendrapathak1988@gmail.com>
2023-01-10 07:06:46 +0530

    added timeline api

--- routes/posts.js
+++ routes/posts.js
@@ -101,13 +101,13 @@ router.get("/timeline/all", async(req,res)=>{
     try {
 
         const currentUser = await User.findById(req.body.userId);
-        const userPost = await User.find({userId: currentUser._id});
+        const userPost = await Post.find({userId: currentUser._id});
         const friendPosts = await Promise.all(
             currentUser.following.map((frindId)=>{
                  return Post.find({userId:frindId});
             })
         );
-        res.status(200).json(userPost.concat(...friendPosts));
+        res.json(userPost.concat(...friendPosts));
         
     } catch (error) {
         res.status(500).json(error);
